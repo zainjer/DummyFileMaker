40e0288d8ac8ca86db9c4d490d63edc7ea1b5d5d
zainjer <arainareeb@gmail.com>
2020-08-29 14:45:08 +0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace DummyFileMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filepath = "c:/dumdum/dummyFile.txt";
+
+            CreateFile(2,filepath);
+
+        }
+
+        static void CreateFile(int sizeInMB, string path)
+        {        
+            string x = "🤣";
+            float countedByte = 5;
+            float calc = (sizeInMB*(1048576/countedByte));
+            float steps= MathF.Round(calc);
+            System.Console.WriteLine("calc:"+calc);
+            System.Console.WriteLine("step:"+steps);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            {
+                int status = 100000;
+                for(int i=0; i<=steps; i++)
+                {
+                    if(status < 1)
+                    {
+                        System.Console.WriteLine("100 kb done");
+                        status = 100000;
+                    }
+                    writer.Write(x);
+
+                    status = status - 5;
+                }
+            }
+        }
+    }
+}
